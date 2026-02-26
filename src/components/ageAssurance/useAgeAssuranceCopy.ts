bluesky-social/social-app

[APP-1764] Improve Age Assurance copy (#9885)
Co-authored-by: surfdude29 <149612116+surfdude29@users.noreply.github.com>

src/components/ageAssurance/useAgeAssuranceCopy.ts
@@ -2,20 +2,29 @@ import {useMemo} from 'react'
 import {msg} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 
+import {useAgeAssurance} from '#/ageAssurance'
+
 export function useAgeAssuranceCopy() {
   const {_} = useLingui()
+  const aa = useAgeAssurance()
+
+  const hasCompletedFlow = aa.state.status === aa.Status.Assured
 
   return useMemo(() => {
     return {
-      notice: _(
-        msg`Due to laws in your region, certain features on Bluesky are currently restricted until you're able to verify you're an adult.`,
-      ),
+      notice: hasCompletedFlow
+        ? _(
+            msg`You have completed the Age Assurance process, but based on the results, we cannot be sure that you are 18 years of age or older. Due to laws in your region, certain features on Bluesky must remain restricted until you're able to verify you're an adult.`,
+          )
+        : _(
+            msg`Due to laws in your region, certain features on Bluesky are currently restricted until you're able to verify you're an adult.`,
+          ),
       banner: _(
-        msg`The laws in your location require you to verify you're an adult to access certain features. Tap to learn more.`,
+        msg`The laws in your region require you to verify you're an adult to access certain features. Tap to learn more.`,
       ),
       chatsInfoText: _(
         msg`Don't worry! All existing messages and settings are saved and will be available after you verify you're an adult.`,
       ),
     }
-  }, [_])
+  }, [_, hasCompletedFlow])
 }

jaredpattison/sheersky

src/components/ageAssurance/useAgeAssuranceCopy.ts
@@ -2,20 +2,29 @@ import {useMemo} from 'react'
 import {msg} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 
+import {useAgeAssurance} from '#/ageAssurance'
+
 export function useAgeAssuranceCopy() {
   const {_} = useLingui()
+  const aa = useAgeAssurance()
+
+  const hasCompletedFlow = aa.state.status === aa.Status.Assured
 
   return useMemo(() => {
     return {
-      notice: _(
-        msg`Due to laws in your region, certain features on SheerSky are currently restricted until you're able to verify you're an adult.`,
-      ),
+      notice: hasCompletedFlow
+        ? _(
+            msg`You have completed the Age Assurance process, but based on the results, we cannot be sure that you are 18 years of age or older. Due to laws in your region, certain features on SheerSky must remain restricted until you're able to verify you're an adult.`,
+          )
+        : _(
+            msg`Due to laws in your region, certain features on SheerSky are currently restricted until you're able to verify you're an adult.`,
+          ),
       banner: _(
-        msg`The laws in your location require you to verify you're an adult to access certain features. Tap to learn more.`,
+        msg`The laws in your region require you to verify you're an adult to access certain features. Tap to learn more.`,
       ),
       chatsInfoText: _(
         msg`Don't worry! All existing messages and settings are saved and will be available after you verify you're an adult.`,
       ),
     }
-  }, [_])
+  }, [_, hasCompletedFlow])
 }